425935a2ed3deb1d59d7fe098a7e3be59c4ae154
Connor Ferster <connorferster@gmail.com>
2022-04-12 22:15:46 -0700

    Initial commit of Pandas part 2

--- my_package/my_package/__init__.py
+++
@@ -1,2 +0,0 @@
-from my_package import nums
-from my_package import months
--- my_package/tests/test_nums.py
+++
@@ -1,4 +0,0 @@
-from my_package import nums
-
-def test_create_number_sequence():
-    assert nums.create_number_sequence(3) == [0, 1, 2, 3]
